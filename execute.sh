2cdaae1a0ed91b165de52fc081db938bcf0ee32b
codehermit <codehermit.algo@gmail.com>
2026-09-21 09:28:23 +0530

    20260921

--- execute.sh
+++ execute.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
-ssh -i '/Users/subhash/AWS/AWS/aws keys/zct.pem' ec2-user@3.111.138.194
+ssh -i '/Users/subhash/AWS/AWS/aws keys/zct.pem' ec2-user@3.109.123.94
+# ssh -i '/Users/subhash/AWS/AWS/aws keys/zct.pem' ec2-user@3.111.138.194
 
 
 # scp -i '/Users/subhash/AWS/AWS/aws keys/zct.pem' -r ec2-user@3.111.138.194:/home/ec2-user/trading/logs .
